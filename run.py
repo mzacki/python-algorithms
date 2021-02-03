0a38625b8a250e883f27260bdcc1002beacaac38
mzacki <hudzikowskim@tipico.com>
2021-02-03 19:01:40 +0100

    quick sort

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from algo.search.binary.binary_search import binary_search
+from algo.sort.quick.quick_sort import quick_sort
 from algo.sort.selection.selection_sort import selection_sort
 
 my_list = [1, 3, 5, 7, 9]
@@ -8,3 +9,4 @@ print(binary_search(my_list, -1))  # => none
 
 unsorted_list = [9, 3, 4, 8, 2, 1]
 print(selection_sort(unsorted_list))  # => returns sorted list
+print(quick_sort([1, 0, 0]))

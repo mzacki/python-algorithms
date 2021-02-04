43db18d8e17692a3de779e606a255e58d9c8ce0b
mzacki <hudzikowskim@tipico.com>
2021-02-04 19:04:51 +0100

    breadth first

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from algo.search.binary.binary_search import binary_search
+from algo.search.breadth_first.breadth_first import search
 from algo.sort.quick.quick_sort import quick_sort
 from algo.sort.selection.selection_sort import selection_sort
 
@@ -10,3 +11,10 @@ print(binary_search(my_list, -1))  # => none
 unsorted_list = [9, 3, 4, 8, 2, 1]
 print(selection_sort(unsorted_list))  # => returns sorted list
 print(quick_sort([1, 0, 0]))
+
+graph = {"start": ["town", "town", "village"], "country": ["road", "path"],
+         "city": ["street"], "capital": ["square", "town_hall"], "lake": [],
+         "forest": [], "end": [], "cul_de_sac": []}
+
+search(graph)
+
